5f2e761ae3be278700cf3dcf07fd3f05a4a0b1f8
Calpurniax <sofia.gracia@gmail.com>
2023-08-26 13:09:47 +0200

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 //import { useState } from 'react'
 import './App.css'
-
-function App() {
- 
+import Header from './components/Header'
+function App() { 
 
   return (
-    <>
-      
-       
-    </>
+   <Header></Header>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+const Header =()=>{
+    return(
+        <h1>hola</h1>
+    )};export default Header 
