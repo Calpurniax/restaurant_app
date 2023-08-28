7c41c36410c835c2aec56cdada73b1ad49e5ec56
Calpurniax <sofia.gracia@gmail.com>
2023-08-28 18:56:18 +0200

    add background img and css

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -8,21 +8,23 @@ import Card from "./Card";
 const List =()=>{
     return(
         <section className="restaurants">
-            <h2 className="restaurants__title">Restaurantes</h2>
-                <ul className="restaurants__list">
-                   <Card
-                   img={restaurant1}
-                   title={'Restaurante de Paca'}
-                   text={'el mejor de todo Carabanchel'}/>
-                   <Card
-                   img={restaurant2}
-                   title={'Restaurante de Clara'}
-                   text={'el mejor de toda la Latina'}/>
-                   <Card
-                   img={restaurant3}
-                   title={'Restaurante de Benita'}
-                   text={'el mejor de todo el barrio de Salamanca'}/>
-                </ul>
+            <div className="restaurants__container">
+                <h2 className="restaurants__title">Restaurantes</h2>
+                    <ul className="restaurants__list">
+                       <Card
+                       img={restaurant1}
+                       title={'Restaurante de Paca'}
+                       text={'el mejor de todo Carabanchel'}/>
+                       <Card
+                       img={restaurant2}
+                       title={'Restaurante de Clara'}
+                       text={'el mejor de toda la Latina'}/>
+                       <Card
+                       img={restaurant3}
+                       title={'Restaurante de Benita'}
+                       text={'el mejor de todo el barrio de Salamanca'}/>
+                    </ul>
+            </div>
         </section>
 )};
 export default List 
