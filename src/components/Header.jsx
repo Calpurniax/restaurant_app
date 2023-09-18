b038ca7ec60f688e682dc95ebb646e102923b3b4
Calpurniax <sofia.gracia@gmail.com>
2023-09-18 21:37:53 +0200

    change titles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "../styles/components/header.scss"
 const Header =()=>{
     return(
         <header className="header">
-            <h1 className="header__title">WELKHOMETech</h1>            
+            <h1 className="header__title">Restaurant App</h1>            
                 <div className="header__searcher">
                 <i className="fa-solid fa-magnifying-glass header__searcher__icon"></i>
                 <input className="header__searcher__input" type="text" name="search" id="search" />
